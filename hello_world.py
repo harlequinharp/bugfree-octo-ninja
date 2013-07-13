2525405b2325e4c8e401363b7a44ed9568793e7c
harlequinharp <harlequinharp@gmail.com>
2013-07-13 17:09:26 -0400

    created my first class

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
-print('Hello, world!')
+def hello_world():
+	print('Hello, world!')
+	
+class ShibaInu:
+	def __init__(self, weight, name):	
+		self.tail='curly'
+		self.ears='pointy'
+		self.weight=weight
+		self.name=name
+
+	def introduce(self):
+		print('HI! My name is {name}!!! My tail is {tail}, my ears are {ears}, and I weigh {weight} pounds.'.format(
+				name=self.name, tail=self.tail, ears=self.ears, weight=self.weight))
+	def beg(self):
+		print('Please could I have some of that?')
+	def howl(self):
+		print('AROooooOOOOOooOoOOoOOOOoooOOOOOOOoooo!')
